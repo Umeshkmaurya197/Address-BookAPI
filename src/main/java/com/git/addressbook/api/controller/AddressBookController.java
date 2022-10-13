32a79942c507f3634ead2c4d3f5b22d50b9762a7
Umesh K Maurya <umeshkumarmaurya.98@gmail.com>
2022-10-13 20:02:39 +0530

    uc 3 -based on USER INTERFACE

--- src/main/java/com/git/addressbook/api/controller/AddressBookController.java
+++ src/main/java/com/git/addressbook/api/controller/AddressBookController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
-
+@CrossOrigin
 @RestController
 @RequestMapping("/address-book")
 public class AddressBookController {
@@ -29,21 +29,21 @@ public class AddressBookController {
     @GetMapping("/get-contact/{contactId}")
     public ResponseEntity<ResponseDTO> getContactById(@PathVariable Long contactId) {
         ResponseDTO responseDTO = new ResponseDTO("Based on contact id " + contactId + " found data ", contactServiceRepository.getContactById(contactId));
-        return new ResponseEntity<>(responseDTO, HttpStatus.FOUND);
+        return new ResponseEntity<>(responseDTO, HttpStatus.OK);
     }
 
     //Curl - http://localhost:8080/address-book/get-all-contacts
     @GetMapping("/get-all-contacts")
     public ResponseEntity<ResponseDTO> getAllContacts() {
         ResponseDTO responseDTO = new ResponseDTO("Based on request found data ", contactServiceRepository.getAllContact());
-        return new ResponseEntity<>(responseDTO, HttpStatus.FOUND);
+        return new ResponseEntity<>(responseDTO, HttpStatus.OK);
     }
 
     //Curl - http://localhost:8080/address-book/get-contact-by-name/Lucifer Morning Star
     @GetMapping("/get-contact-by-name/{fullName}")
     public ResponseEntity<ResponseDTO> getContactByName(@PathVariable String fullName) {
         ResponseDTO responseDTO = new ResponseDTO("Based on Contact Name " + fullName + " found data", contactServiceRepository.getContactByName(fullName));
-        return new ResponseEntity<>(responseDTO, HttpStatus.FOUND);
+        return new ResponseEntity<>(responseDTO, HttpStatus.OK);
     }
 
     //Curl - http://localhost:8080/address-book/update-contact/1
